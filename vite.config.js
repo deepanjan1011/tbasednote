3d1c42044f7cc3d885912e978102e33ed4eda74c
deepanjan1011 <deepanjanpati11@gmail.com>
2026-01-14 08:11:59 +0530

    fix: update PWA icon versioning in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,13 +19,13 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png?v=2',
+            src: 'pwa-192x192.png?v=3',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: 'pwa-512x512.png?v=2',
+            src: 'pwa-512x512.png?v=3',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
